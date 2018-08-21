4efc944a27dff32567933d23bc0da5a18eac7649
bang <leowang46796047@gmail.com>
2018-08-21 15:59:32 +0800

    upload

---
+++ upload.py
@@ -0,0 +1,19 @@
+git config --global user.name "bang"
+git config --global user.email leowang46796047@gmail.com
+
+
+
+
+
+echo“#lkjhgfdsa”>> README.md 
+git init 
+git add README.md 
+git commit -m“新增README.md檔” 
+git remote add origin https://github.com/jiawpipi1/lkjhgfdsa.git
+git push -u origin master
+
+
+
+git add README.md 
+git commit -m“新增README.md檔” 
+git push -u origin master
