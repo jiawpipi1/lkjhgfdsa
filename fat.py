ee9ae14dbabb079789405c55f0652720acd0ac3f
bang <leowang46796047@gmail.com>
2018-08-21 14:54:01 +0800

    新增fat.py檔

---
+++ fat.py
@@ -0,0 +1,12 @@
+x = 'yes'
+age = int(input('請輸入年齡:'))
+weight = int(input('請輸入體重:'))
+y = input('你是男的嗎(yes/no)')
+if age>= 20 and weight >= 100 and y == x:
+    print('你可以當相撲選手')
+else:
+    print('你不能當相撲選手')
+
+
+
+
