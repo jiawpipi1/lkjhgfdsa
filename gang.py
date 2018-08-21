46d43b37771a6c130790f16e3cf94c44c6905f47
bang <leowang46796047@gmail.com>
2018-08-21 14:54:58 +0800

    新增adult檔

---
+++ gang.py
@@ -0,0 +1,6 @@
+x = 'yes'
+y = input('Are you over 18 years old?')
+if y==x :
+    print('Welcome to the lovely adult world:www.pornhub.com')
+else:
+    print('Welcome to the children`s paradise:www.pornhub.com')
