759ff0101ef7e4fefb2f77f2384850fa216c0a2f
bang <leowang46796047@gmail.com>
2018-08-22 09:35:07 +0800

    gang.py

---
+++ bann.py
@@ -0,0 +1,8 @@
+import webbrowser
+y = input('Are you over 18 years old?')
+if y=='yes' :
+    print('Welcome to the lovely adult world')
+    webbrowser.open('http://www.pornhub.com')
+else:
+    print('Welcome to the children`s paradise')
+    webbrowser.open('http://www.pornhub.com')
